defa67103f68796bf8c6cf176959611d0a0a8c76
AndreyAndreyQ11 <aayy6491@gmail.com>
2022-11-18 15:07:50 +0200

    t1

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+// import Quest_1 from "./Quest_1/Quest_1";
+// import Quest_2 from "./Quest_2/Quest_2";
+// import Quest_2_v2 from "./Quest_2_v2/Quest_2_v2";
+// import Quest_3 from "./Quest_3/Quest_3";
+import Counter from "./Counter/Counter";
+
+import "./App.css";
+// import data from "../data/data";
+// import friends from "../data/friends";
+// import transactions from "../data/transactions";
+
+export const App = () => {
+  return (
+    <>
+      <Counter />
+
+      {/* <Quest_3
+        id={transactions.map(el => el.id)}
+        type={transactions.map(el => el.type)}
+        amount={transactions.map(el => el.amount)}
+        currency={transactions.map(el => el.currency)}
+      /> */}
+    </>
+  );
+};
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Counter.css"
+
+class Counter extends React.Component
+
+const Counter = () => (
+
+    
+);
