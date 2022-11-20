396a725ac044c405bd4dad6f9f11072ce78a92ce
AndreyAndreyQ11 <aayy6491@gmail.com>
2022-11-20 11:57:11 +0200

    q1

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,9 +1,30 @@
 import React from "react";
-import "./Counter.css"
+import s from "./Counter.css"
 
-class Counter extends React.Component
+class Counter extends React.Component {
 
-const Counter = () => (
+    inkrement = (el) => {
+        console.log("увеличить");
+        console.log(el);
+    }
+    denkrement = () => {
+        console.log("уменьшить");
+    }
 
-    
-);
+
+
+
+    render() {
+        return (
+            < div className={s.counter} >
+                < span className={s.value} > 0</span >
+                <div className={s.controls}>
+                    <button onClick={this.inkrement} >+ 1</button>
+                    <button onClick={this.denkrement} >- 1</button>
+                </div>
+            </div >
+        )
+    }
+};
+
+export default Counter;
